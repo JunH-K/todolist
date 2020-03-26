d696f80fb70c0fab38fc3894f893a997bd1a03f4
JunH-K <k.developer@kakao.com>
2020-03-26 16:31:55 +0900

    feat : component index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,1 +1,4 @@
 export { default as Alert } from './alert/Alert';
+export { default as Header } from './header/Header';
+export { default as Button } from './form/Button';
+export { default as InputText } from './form/InputText';
