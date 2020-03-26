ba273a792b1c802d5208d3b5e1ec6246d0a22c41
JunH-K <k.developer@kakao.com>
2020-03-26 11:21:48 +0900

    feat : todolist 호출 reducer

---
+++ src/reducers/todos.js
@@ -0,0 +1,20 @@
+import { handleActions } from 'redux-actions';
+
+export const TODO_LIST_REQUEST = 'todo/TODO_LIST_REQUEST';
+export const TODO_LIST_SUCCESS = 'todo/TODO_LIST_SUCCESS';
+export const TODO_LIST_ERROR = 'todo/TODO_LIST';
+
+const defaultState = { todoList: [{ title: '제목' }] };
+
+const todos = handleActions(
+  {
+    [TODO_LIST_SUCCESS]: action => {
+      return {
+        todoList: action.data,
+      };
+    },
+  },
+  defaultState
+);
+
+export default todos;
