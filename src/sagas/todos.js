b2bcc53a4bde0083c96b240545601255e7b3f9c7
JunH-K <k.developer@kakao.com>
2020-03-28 16:52:33 +0900

    feat : todo 추가 saga api 연결

--- src/sagas/todos.js
+++ src/sagas/todos.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import {
   TODO_LIST_REQUEST,
   TODO_LIST_SUCCESS,
-  TODO_LIST_ERROR,
+  TODO_LIST_ERROR, ADD_TODO,
 } from '../reducers/todos';
 
 function todoListAPI(page) {
@@ -31,6 +31,32 @@ function* watchTodoList() {
   yield takeLatest(TODO_LIST_REQUEST, todoList);
 }
 
+function AddTodoAPI(todo) {
+  console.log( todo );
+  return axios.post(`/todos`,todo);
+}
+
+function* addTodo(action) {
+  try {
+    const result = yield call(AddTodoAPI, action.data);
+
+    // yield put({
+    //   type: TODO_LIST_SUCCESS,
+    //   data: result.data,
+    // });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: TODO_LIST_ERROR,
+      error: e,
+    });
+  }
+}
+
+function* watchAddTodo() {
+  yield takeLatest(ADD_TODO, addTodo);
+}
+
 export default function* todoSaga() {
-  yield all([fork(watchTodoList)]);
+  yield all([fork(watchTodoList),fork(watchAddTodo)]);
 }
