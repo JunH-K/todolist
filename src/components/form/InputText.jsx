237b47f441ee300bd536829c8bc7d6946a031973
JunH-K <k.developer@kakao.com>
2020-03-28 16:52:53 +0900

    feat : todo 추가 화면-api 연결

--- src/components/form/InputText.jsx
+++ src/components/form/InputText.jsx
@@ -1,12 +1,18 @@
 import { InputTextStyle } from './Style';
 import React from 'react';
 
-const InputText = ({ className, placeholder = '' }) => {
+const InputText = (
+  { className, placeholder = '', onChange, defaultValue },
+  ...rest
+) => {
   return (
     <InputTextStyle
       type="text"
       className={className}
       placeholder={placeholder}
+      onChange={onChange}
+      defaultValue={defaultValue}
+      {...rest}
     />
   );
 };
--- src/components/write/Write.jsx
+++ src/components/write/Write.jsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import WriteStyle from './Style';
 import { Button, InputText } from '..';
+import { useDispatch } from 'react-redux';
+import { ADD_TODO } from '../../reducers/todos';
 
-const Write = ({
-  todoValue,
-  refValue,
-  completeText,
-  onClickComplete,
-  children,
-}) => {
+const Write = ({ todoValue, refValue, completeText, children }) => {
+  const dispatch = useDispatch();
+  const [todoText, setTodoText] = useState('');
+  const [todoRefId, setTodoRefId] = useState('');
+
+  const onChangeTodo = useCallback(e => {
+    setTodoText(e.target.value);
+  }, []);
+
+  const onChangeRefId = useCallback(e => {
+    setTodoRefId(e.target.value);
+  }, []);
+
+  const onClickAddTodo = useCallback(() => {
+    dispatch({
+      type: ADD_TODO,
+      data: {
+        content: todoText,
+        refId: todoRefId,
+        done: false,
+      },
+    });
+  }, [todoText, todoRefId]);
+
   return (
     <WriteStyle>
       <div className="write-todo">
-        <InputText type="text" placeholder="할일입력.." />
+        <InputText
+          type="text"
+          placeholder="할일입력.."
+          onChange={onChangeTodo}
+          defaultValue={todoText}
+        />
         <Button
           type="button"
           className="add"
-          onClick={onClickComplete}
+          onClick={onClickAddTodo}
           name={completeText}
         />
       </div>
       <div className="write-ref">
-        <InputText type="text" placeholder="참조 Todo 아이디입력.. ex) @1@5" />
+        <InputText
+          type="text"
+          placeholder="참조 Todo 아이디입력.. ex) @1@5"
+          onChange={onChangeRefId}
+          defaultValue={todoRefId}
+        />
         <Button type="button" className="cancel" name={'취소'} />
         <Button type="button" className="delete" name={'삭제'} />
       </div>
