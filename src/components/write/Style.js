b5a4dd4d810db3b69cc96b193f99e2ab72e398f2
JunH-K <k.developer@kakao.com>
2020-03-28 13:44:41 +0900

    feat : write media css 추가

--- src/components/write/Style.js
+++ src/components/write/Style.js
@@ -39,6 +39,14 @@ const Write = styled.div`
   .delete {
     width: 11%;
   }
+  
+  @media (max-width: 768px) {  
+    margin: 0;
+    border-top:0;
+    border-left:0;
+    border-right:0;
+   }
+  
 `;
 
 export default Write;
