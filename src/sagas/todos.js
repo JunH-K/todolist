13e5dc86b260da9cbd39e7d920f470438505b123
JunH-K <k.developer@kakao.com>
2020-03-28 17:28:42 +0900

    feat : 할일추가후 할일리스트 재요청

--- src/sagas/todos.js
+++ src/sagas/todos.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 import {
   TODO_LIST_REQUEST,
   TODO_LIST_SUCCESS,
-  TODO_LIST_ERROR, ADD_TODO,
+  TODO_LIST_ERROR,
+  ADD_TODO,
+  ADD_TODO_ERROR,
 } from '../reducers/todos';
 
 function todoListAPI(page) {
@@ -32,22 +34,21 @@ function* watchTodoList() {
 }
 
 function AddTodoAPI(todo) {
-  console.log( todo );
-  return axios.post(`/todos`,todo);
+  return axios.post(`/todos`, todo);
 }
 
 function* addTodo(action) {
   try {
     const result = yield call(AddTodoAPI, action.data);
 
-    // yield put({
-    //   type: TODO_LIST_SUCCESS,
-    //   data: result.data,
-    // });
+    yield put({
+      type: TODO_LIST_REQUEST,
+      data: result.data,
+    });
   } catch (e) {
     console.error(e);
     yield put({
-      type: TODO_LIST_ERROR,
+      type: ADD_TODO_ERROR,
       error: e,
     });
   }
@@ -58,5 +59,5 @@ function* watchAddTodo() {
 }
 
 export default function* todoSaga() {
-  yield all([fork(watchTodoList),fork(watchAddTodo)]);
+  yield all([fork(watchTodoList), fork(watchAddTodo)]);
 }
