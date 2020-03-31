0c83ed7849e37471702ae61f63fec4a623b1a75d
JunH-K <k.developer@kakao.com>
2020-03-31 16:40:44 +0900

    feat : querystring 처리 로직 추가

--- src/containers/todoContainer/TodoContainer.jsx
+++ src/containers/todoContainer/TodoContainer.jsx
@@ -1,24 +1,38 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
 import { TODO_LIST_REQUEST } from '../../reducers/todos';
 import { Header, Pagination } from '../../components';
 import Container from './Style';
 import WriteContainer from '../writeContainer/WriteContainer';
 import TodoListContainer from '../todoListContainer/TodoListContainer';
 
-const TodoContainer = props => {
-  const {
-    match: {
-      params: { page = 1 },
-    },
-  } = props;
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
 
+const getQuey = querys => {
+  const query = useQuery();
+  const queryObj = {};
+  querys.forEach(queryString => {
+    queryObj[queryString] = query.get(queryString) || '';
+  });
+  return queryObj;
+};
+
+const TodoContainer = () => {
+  const { page = 1 } = useParams();
+  const location = useLocation();
   const dispatch = useDispatch();
   const { todoList, pageInfo } = useSelector(state => state.todos);
+  const queryStrings = {
+    page,
+    ...getQuey(['searchText', 'sort', 'order', 'done', 'searchId']),
+  };
 
   useEffect(() => {
-    dispatch({ type: TODO_LIST_REQUEST, data: { page } });
-  }, [page]);
+    dispatch({ type: TODO_LIST_REQUEST, data: queryStrings });
+  }, [location]);
 
   return (
     <>
