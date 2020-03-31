b5cddd36a537537ab92fbc54cec3fadd2f9b6157
JunH-K <k.developer@kakao.com>
2020-03-31 16:39:38 +0900

    feat : 검색 구현 , 500밀리초후 자동 검색 엔터검색

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import querystring from 'querystring';
 import { HeaderStyle, MoreActionStyle } from './Style';
+import { useHistory } from 'react-router-dom';
 import InputText from '../form/InputText';
 import { Button } from '..';
 import MoreAction from './MoreAction';
+import { useDispatch } from 'react-redux';
+import { TODO_LIST_REQUEST } from '../../reducers/todos';
 
 const Header = props => {
+  const history = useHistory();
+  const [search, setSearch] = useState('');
+  const timer = useRef(0);
+
+  const request = value => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+    timer.current = setTimeout(() => {
+      let query = {};
+      if (value) {
+        query = {
+          searchText: value,
+        };
+      }
+
+      history.push(`./1?${querystring.stringify(query)}`);
+    }, 500);
+  };
+
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e;
+    setSearch(value);
+    request(value);
+  };
+
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      request(search);
+    }
+  };
+
   return (
     <HeaderStyle>
       <InputText
         className="search"
-        placeholder="검색 ex) 내용 , 날짜:20200101"
+        placeholder="검색 ex) 내용 , @1"
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        value={search}
       />
       <Button type="button" className="more-action" name="더보기" />
-      {/*<MoreAction />*/}
+      {/* <MoreAction /> */}
     </HeaderStyle>
   );
 };
