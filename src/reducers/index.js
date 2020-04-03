0351347d350d5e607f7f4f069cc6e2a9b777ff49
JunH-K <k.developer@kakao.com>
2020-04-03 23:24:19 +0900

    delete : datamanager reducer saga 삭제

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import todos from './todos';
-import dataManager from './dataManager';
 
 const rootReducer = combineReducers({
   todos,
-  dataManager,
 });
 
 export default rootReducer;
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 import todos from './todos';
-import dataManager from './dataManager';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://todo-server-nine.now.sh/';
 export default function* rootSaga() {
-  yield all([call(todos), call(dataManager)]);
+  yield all([call(todos)]);
 }
