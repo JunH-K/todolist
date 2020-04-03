37bf768406310bad233a49f1250a86d7559c5b12
JunH-K <k.developer@kakao.com>
2020-04-03 17:51:15 +0900

    feat :백업데이터 호출 api saga 생성

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,9 @@
 import { all, call } from 'redux-saga/effects';
 import todos from './todos';
+import dataManager from './dataManager';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://todo-server-nine.now.sh/';
 export default function* rootSaga() {
-  yield all([call(todos)]);
+  yield all([call(todos), call(dataManager)]);
 }
