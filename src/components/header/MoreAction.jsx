a2ffc938938b5955e1e372e20bad7939158bc94a
JunH-K <k.developer@kakao.com>
2020-04-04 11:56:13 +0900

    feat :데이터백업,복원 - 컨테이너로 분리

--- src/components/header/MoreAction.jsx
+++ src/components/header/MoreAction.jsx
@@ -1,12 +1,10 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import querystring from 'querystring';
-import axios from 'axios';
 import { MoreActionStyle, MoreActonContainerStyle } from './Style';
-import { showToast } from '..';
-import { ADD_TODO_REQUEST } from '../../reducers/todos';
+import DataManagerContainer from '../../containers/dataManagerContainer/DataManagerContainer';
 
 const menus = [
   { name: '생성일 오름차순', key: 'createdAsc' },
@@ -32,11 +30,8 @@ const menusQuery = {
 };
 
 const MoreAction = ({ onClickOutside }) => {
-  const dispatch = useDispatch();
   const history = useHistory();
   const { queryString } = useSelector(state => state.todos);
-  const download = useRef(null);
-  const restore = useRef(null);
 
   const combineQueryString = useCallback(
     menu => {
@@ -75,44 +70,6 @@ const MoreAction = ({ onClickOutside }) => {
     history.push(`/page/1?${querystring.stringify(string)}`);
   }, []);
 
-  const fetchBackupData = async () => {
-    try {
-      showToast('백업중..');
-      const backupData = await axios.get('/todos');
-      return backupData.data.row;
-    } catch (error) {
-      console.error(error);
-      showToast('백업실패');
-    }
-  };
-
-  const onClickBackup = () => {
-    fetchBackupData().then(backupData => {
-      download.current.href = `data:text/json;charset=utf-8,${encodeURIComponent(
-        JSON.stringify(backupData)
-      )}`;
-      download.current.click();
-    });
-  };
-
-  const onClickRestore = () => {
-    restore.current.click();
-  };
-
-  const onChangeHandleFiles = e => {
-    const [file] = e.target.files;
-    if (file) {
-      file.text().then(data => {
-        const backup = JSON.parse(data);
-        showToast('데이터 복원 중...');
-        dispatch({
-          type: ADD_TODO_REQUEST,
-          data: backup,
-        });
-      });
-    }
-  };
-
   return (
     <MoreActonContainerStyle onClick={onClickOutside} data-name="outside">
       <MoreActionStyle className="more-action-menu" onClick={onClickMenuItem}>
@@ -126,33 +83,7 @@ const MoreAction = ({ onClickOutside }) => {
             </div>
           );
         })}
-        <a
-          id="download"
-          download="backupTodo.json"
-          ref={download}
-          style={{ display: 'none' }}
-        >
-          데이터백업
-        </a>
-        <div onClick={onClickBackup} className="menu">
-          데이터 백업
-        </div>
-        <input
-          type="file"
-          id="fileElem"
-          multiple
-          accept="application/json"
-          style={{ display: 'none' }}
-          ref={restore}
-          onChange={onChangeHandleFiles}
-        />
-        <div
-          onClick={onClickRestore}
-          className="menu"
-          title="할일이 복원파일로 대체됩니다."
-        >
-          데이터 복원
-        </div>
+        <DataManagerContainer />
       </MoreActionStyle>
     </MoreActonContainerStyle>
   );
